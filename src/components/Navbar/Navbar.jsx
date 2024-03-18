899f21a2bfee55a0c36351679e4c665751293841
Ridwan4D <ridwanul161@gmail.com>
2024-03-19 02:49:38 +0600

    menu and close icon is toggled now

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import Link from "./Link";
+import { HiMenuAlt1 } from "react-icons/hi";
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   const routes = [
-    {name: "Home", id: "home_page_123", path: "/home" },
-    {name: "About", id: "about_page_456", path: "/about" },
-    {name: "Services", id: "services_page_789", path: "/services"},
-    {name: "Contact", id: "contact_page_101", path: "/contact" },
-    {name: "Portfolio",id: "portfolio_page_202",path: "/portfolio"},
+    { name: "Home", id: "home_page_123", path: "/home" },
+    { name: "About", id: "about_page_456", path: "/about" },
+    { name: "Services", id: "services_page_789", path: "/services" },
+    { name: "Contact", id: "contact_page_101", path: "/contact" },
+    { name: "Portfolio", id: "portfolio_page_202", path: "/portfolio" },
   ];
 
   return (
     <nav>
+      <div className="md:hidden text-2xl" onClick={() => setOpen(!open)}>
+        {open === true ? (
+          <IoMdCloseCircleOutline></IoMdCloseCircleOutline>
+        ) : (
+          <HiMenuAlt1></HiMenuAlt1>
+        )}
+      </div>
       <ul className="md:flex">
         {routes.map((route) => (
-         <Link key={route.id} route={route}></Link>
+          <Link key={route.id} route={route}></Link>
         ))}
       </ul>
     </nav>
